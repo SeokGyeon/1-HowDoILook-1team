0ffe0c0037ba9732f1a43844550637e95604d982
Gyeon <sugki5566@gmail.com>
2025-02-27 13:24:18 +0900

    delete /

--- config/database.js
+++ config/database.js
@@ -9,9 +9,9 @@ const connectDB = async () => {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log("✅ MongoDB 연결 성공");
+    console.log("MongoDB 연결 성공");
   } catch (error) {
-    console.error("❌ MongoDB 연결 실패:", error);
+    console.error("MongoDB 연결 실패:", error);
     process.exit(1);
   }
 };
