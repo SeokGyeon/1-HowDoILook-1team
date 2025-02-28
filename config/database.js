7b10ced89ec5d2a01b57cf9df699472bea334186
SeokGyeon <tjrrl1623@naver.com>
2025-02-28 14:56:10 +0900

    Update database.js
    안정성과 최적화를 위해 사용한다고 찾아보았으나 아래의 메세지를 확인하고 `useNewUrlParser: true, useUnifiedTopology: true, ` 내용 삭제.
    (node:1580) [MONGODB DRIVER] Warning: useNewUrlParser is a deprecated option: useNewUrlParser has no effect since Node.js Driver version 4.0.0 and will be removed in the next major version
    (Use `node --trace-warnings ...` to show where the warning was created)
    (node:1580) [MONGODB DRIVER] Warning: useUnifiedTopology is a deprecated option: useUnifiedTopology has no effect since Node.js Driver version 4.0.0 and will be removed in the next major version.
    
    해당 부분을 지우니 warning 없이 실행되는 것을 확인

--- config/database.js
+++ config/database.js
@@ -5,11 +5,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      //안정성 및 최적화를 위해 사용한다 함
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB 연결 성공");
   } catch (error) {
     console.error("MongoDB 연결 실패:", error);
