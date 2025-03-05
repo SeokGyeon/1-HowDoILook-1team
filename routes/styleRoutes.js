07f2cabdffbdfb214908eb4d979a1136b37f01b3
Gyeon <sugki5566@gmail.com>
2025-03-05 17:24:21 +0900

    Add search, get, ranking, reposition

--- routes/styleRoutes.js
+++ routes/styleRoutes.js
@@ -6,6 +6,7 @@ import {
   updateStyle,
   deleteStyle,
   getStyleDetail,
+  getStyleById,
 } from "../controllers/styleController.js";
 
 const router = express.Router();
@@ -15,6 +16,7 @@ router.post("/", createStyle);
 router.get("/", getStyles);
 router.patch("/:id", updateStyle);
 router.delete("/:id", deleteStyle);
+router.get("/:id", getStyleById);
 router.get("/:id", getStyleDetail);
 
 export default router;
