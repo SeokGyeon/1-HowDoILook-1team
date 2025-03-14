c65435277b7deb9ddb7e335c8d1e8df3b89bef24
Nanobel <85973198+Nanobel@users.noreply.github.com>
2025-03-14 14:28:30 +0900

    원래 있던 파일 삭제

--- src/routes/images.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-const router = express.Router();
-import { uploadImage } from "../controllers/imageController.js";
-
-// 이미지 업로드
-router.post("/upload", uploadImage);
-
-export default router;
--- src/routes/tags.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-const router = express.Router();
-import { getPopularTags } from "../controllers/tagController.js";
-
-// 인기 태그 목록 조회
-router.get("/popular-tags", getPopularTags);
-
-export default router;
